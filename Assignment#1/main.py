9d2377ff5215d2aeb615f90f21f7d36347f685a1
Farzam Ahmed <you@example.com>
2021-12-31 22:17:19 +0500

    Work Accomplish

---
+++ Assignment#1/main.py
@@ -0,0 +1,36 @@
+#Q1
+'''print("Twinkle,twinkle,little star,")
+print("        How i wonder what you are!")
+print("                Up above the world so high,")
+print("                Like a diamond in the sky.")
+print("Twinkle,twinkle,little star,")
+print("        How i wonder what you are")'''
+
+#Q2
+'''import sys
+print("Python version is: ")
+print(sys.version)'''
+
+#Q3
+'''import datetime
+curr=datetime.datetime.now()
+print(curr.strftime("%Y-%m-%d %H:%M:%S"))'''
+
+#Q4
+'''import math
+r=eval(input("Enter Radius of circle:"))
+A=math.pi*r**2
+print(A)'''
+
+#Q5
+'''f_name=str(input("Enter Your First Name:"))
+l_name=str(input("Enter Your Last Name: "))
+r=(l_name + " " +f_name)
+print(r)'''
+
+#Q6
+'''a=eval(input("Enter First Number:"))
+b=eval(input("Enter Second Number:"))
+c=a+b
+print(c)'''
+
